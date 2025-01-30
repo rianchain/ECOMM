ef7c4d96fc8de673699d549d7810d9726d33f826
rianchain <rianziwalker@gmail.com>
2025-01-30 19:58:31 +0700

    add SetupPage component and Modal integration with Radix UI

--- adminsite/app/(root)/page.tsx
+++ adminsite/app/(root)/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+
 import Image from "next/image";
 import { UserButton } from "@clerk/nextjs";
+import Modal from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
-export default function Home() {
-  return (
-    <div className="p-4">
-      <UserButton />
-    </div>
-  );
-}
+const SetupPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
+};
+
+export default SetupPage;
