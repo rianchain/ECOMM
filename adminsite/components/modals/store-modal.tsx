4d9a2ff48a0d0f4cf88ad4aa7c22030a28724e49
rianchain <rianziwalker@gmail.com>
2025-01-31 16:42:55 +0700

    add: implement toast notifications in StoreModal and add ToasterProvider component

--- adminsite/components/modals/store-modal.tsx
+++ adminsite/components/modals/store-modal.tsx
@@ -18,6 +18,7 @@ import {
 } from "../ui/form";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import toast from "react-hot-toast";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -42,8 +43,11 @@ export const StoreModal = () => {
       const response = await axios.post("/api/stores", values);
 
       console.log(response.data);
+      toast.success("Berhasil Membuat Toko");
     } catch (error) {
-      console.log(error);
+      toast.error("Gagal Membuat Toko");
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -65,17 +69,27 @@ export const StoreModal = () => {
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input placeholder="Nama Toko" {...field} />
+                      <Input
+                        placeholder="Nama Toko"
+                        {...field}
+                        disabled={loading}
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
               <div className="pt-6 space-x-2 flex items-center justify-end w-full">
-                <Button variant="outline" onClick={storeModal.onClose}>
+                <Button
+                  disabled={loading}
+                  variant="outline"
+                  onClick={storeModal.onClose}
+                >
                   Cancel
                 </Button>
-                <Button type="submit">Continue</Button>
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
               </div>
             </form>
           </Form>
