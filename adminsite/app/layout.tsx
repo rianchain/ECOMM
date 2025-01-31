e33ba5e25c7da9d7e0a1b8c424d268b146579fff
rianchain <rianziwalker@gmail.com>
2025-01-31 16:42:45 +0700

    add: integrate ToasterProvider for toast notifications in layout

--- adminsite/app/layout.tsx
+++ adminsite/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
-import { ModalProvider } from "@/provider/modal-provider";
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
